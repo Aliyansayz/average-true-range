15272fb8d5d49241be1eed94963c925ca94f6dcd
Aliyansayz <56589240+Aliyansayz@users.noreply.github.com>
2023-04-13 17:24:55 +0500

    Update atr_bands.py

--- atr_bands.py
+++ atr_bands.py
@@ -1,4 +1,12 @@
+def sma (array, period ):
 
+    sma = np.empty_like(array)
+    sma = np.full( sma.shape , np.nan)
+    # Calculate the EMA for each window of 14 values
+    for i in range(period, len(array)+1 ):
+          sma[i-1] = np.mean(array[i-period:i] , dtype=np.float16)
+    return sma 
+     
 def  atr_bands( bar , multiplier ):
 
   close = np.array( bars["Close"] ,dtype=np.float16  )
