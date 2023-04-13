1ccc6d8a32846819ec9c347c3e2a63c670e047b0
Aliyansayz <56589240+Aliyansayz@users.noreply.github.com>
2023-04-13 17:51:27 +0500

    Update atr.py

--- atr.py
+++ atr.py
@@ -1,3 +1,9 @@
+
+""" 
+## Example 
+bar = yfinance or inclusive bar["High"]  bar["Low"] bar["Close"] 
+avg_true_range    =   atr ( bar , period=14  )
+"""
 def sma (array, period ):
 
     sma = np.empty_like(array)
@@ -8,15 +14,14 @@ def sma (array, period ):
     return sma 
      
 
-
-def atr ( bar ):
+def atr ( bar , period  ):
   
   high_low, high_close, low_close  = np.array(bars["High"]-bars["Low"],dtype=np.float16 ) , 
   np.array(abs(bars["High"]-bars["Close"].shift()),dtype=np.float16 ) , 
     np.array(abs(bars["Low"]-bars["Close"].shift() ),dtype=np.float16 )
     
   true_range = np.amax (np.hstack( (high_low, high_close, low_close) ).reshape(-1,3),axis=1 )
-  avg_true_range = sma(true_range , window_size=14)  # takes average for 14 period  
+  avg_true_range = sma(true_range , period )  # takes average for 14 period  
   return avg_true_range 
 
 # returning "true_range" is optional not needed. 
